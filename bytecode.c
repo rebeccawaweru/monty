c2af24852d7d68e2da720c6370c12d36a9cb4d91
rebeccawaweru <wawerur95@gmail.com>
2023-05-24 09:45:39 -0700

    trial update

--- bytecode.c
+++ bytecode.c
@@ -9,13 +9,19 @@ void bytecode_exec(FILE *file)
 	char line[MAX_LENGTH];
 	unsigned int num_line = 0;
 	char *opcode = NULL;
+	size_t len_line;
 
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		num_line++;
+		len_line = strlen(line);
+		if (line[len_line - 1] == '\n')
+			line[len_line - 1] = '\0';
 		opcode = token_acq(line, num_line);
 		if (opcode != NULL)
 			op_instruction(opcode, &stack, num_line);
+		else
+			continue;
 	}
 	free_mem_stack(stack);
 	fclose(file);
